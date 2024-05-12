4d03017e7ce2ea806cbe97cc100b0f192aa04f75
TahaManna07 <123somanna@gmail.com>
2024-05-12 15:43:47 +0100

    g

--- src/main/java/ma/enset/digitalbanking/web/CustomerRestController.java
+++ src/main/java/ma/enset/digitalbanking/web/CustomerRestController.java
@@ -7,6 +7,7 @@ import ma.enset.digitalbanking.dtos.CustomerDTO;
 import ma.enset.digitalbanking.entities.Customer;
 import ma.enset.digitalbanking.exceptions.CustomerException;
 import ma.enset.digitalbanking.services.BankAccountService;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -18,25 +19,34 @@ import java.util.List;
 public class CustomerRestController {
     private BankAccountService bankAccountService;
     @GetMapping("/customers")
+    @PreAuthorize("hasAuthority('SCOPE_USER')")
     public List<CustomerDTO> customer(){
         return bankAccountService.listCostumer();
     }
 
     @GetMapping("/customers/{id}")
+    @PreAuthorize("hasAuthority('SCOPE_USER')")
+
     public CustomerDTO getCustomer(@PathVariable(name = "id") Long customerId) throws CustomerException {
         return  bankAccountService.getCustomer(customerId);
     }
     @PostMapping("/customers")
+    @PreAuthorize("hasAuthority('SCOPE_ADMIN')")
+
     public CustomerDTO saveCustomer(@RequestBody CustomerDTO customerDTO){
         return  bankAccountService.saveCustomer(customerDTO);
     }
     @PutMapping("/customers/{customerId}")
+    @PreAuthorize("hasAuthority('SCOPE_ADMIN')")
+
     public CustomerDTO updateCustomer(@PathVariable Long customerId ,@RequestBody CustomerDTO customerDTO){
         customerDTO.setId(customerId);
         return bankAccountService.updateCustomer(customerDTO);
 
     }
     @DeleteMapping("/customers/{id}")
+    @PreAuthorize("hasAuthority('SCOPE_ADMIN')")
+
     public void deleteCustomer(@PathVariable Long id){
             bankAccountService.deleteCustomer(id);
     }
